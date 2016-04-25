e6dd8728c56d5c63e22e8a25471b0adf982f690c
Austin Morrell <austinlm19@gmail.com>
2016-04-25 16:27:24 -0500

    Im starting to get it

--- Astar.py
+++ Astar.py
@@ -13,10 +13,13 @@ class Node:
 		self.f = None
 		self.g = None
 		self.h = None
+		self.color = (0, 0, 255) if (self.walkable) else (255,0,0)
+		self.adjacents = []
+		self.goal = None
 
 	def draw(self, screen, color):
 		margin = self.margin
-		color = (0, 0, 255) if (self.walkable) else (255,0,0)
+		color = self.color
 		gfx.draw.rect(screen, color, (self.left , self.top, self.width, self.height))
 		
 	def setWalk(self, walkable):
@@ -24,24 +27,84 @@ class Node:
 		 
 	def getF(self):
 		return self.h + self.g
-	def setH(self, val):
-		self.h = val
+	def setH(self, Goal):
+		self.goal = Goal
+		numb = (self.goal.pos[0] - self.pos[0]) + (self.goal.pos[1] - self.pos[1])
+		self.h = numb
 	def setG(self, val):
 		self.g = val
+	def setColor(self, val):
+		self.color = val
 
 class Astar:
-	def __init__(self, SearchSpace, Start, Goal):
+	def __init__(self, SearchSpace, Start, Goal, dims):
 		self.OPEN = []
 		self.CLOSED = []
-		self.width = 20
-		self.height = 20
-		self.margin = 5
-		self.left = (self.margin + self.width) *  x + self.margin
-		self.top = (self.margin + self.height) *  y + self.margin
-		self.pos = (x, self.height - y)
+		self.space = SearchSpace
+		self._start = Start
+		self._goal = Goal
+		self._current = self._start
+		self.rows = dims[0]
+		self.cols = dims[1]
+	
+	def current(self):
+		return self._current
 		
-	def draw(self, screen, color):
-		margin = self.margin
-		color = (0, 255, 0)
-		gfx.draw.rect(screen, color, (self.left , self.top, self.width, self.height))
+	def current(self, value):
+		self._current = value
 		
+	#def draw(self, screen, color):
+		
+		
+	def Run(self):
+		#self.Reset()
+		open = self.OPEN
+		closed = self.CLOSED
+		start = self._start
+		goal = self._goal
+		open.append(start)
+		print(open)
+		while open:						
+			open.sort(key = lambda x : x.f)
+			current = open[0]
+			open.remove(current)			
+			closed.append(current)
+			i = 0
+			for adj in current.adjacents:
+				if adj.walkable and adj not in closed:
+					if adj not in open:
+						open.append(adj)
+						adj.parent = current						
+						adj.g = 10 if i < 4 else 14
+					else:
+						move = 10 if i < 4 else 14
+						movecost = move + current.g
+						if movecost < adj.g: 
+							adj.parent = current						
+							adj.g = movecost
+							
+				i+=1
+				
+	def GetPath(self, node):
+		path = []
+		current = node
+		while(current != self._start):
+			path.append(current.parent)
+			current = current.parent
+			current.color = (0, 255, 0)
+		return path
+		
+	def SetNeighbors(self):
+		Location = 0
+		for a in range(99):
+			if self.space[a] == self._current:
+				Location = a;
+		top = self.space[Location - 10]
+		bot = self.space[Location + 10]
+		left = self.space[Location - 1]
+		right = self.space[Location + 1]
+		b_left = self.space[Location +9]
+		b_right = self.space[Location + 11]
+		t_left = self.space[Location - 11]
+		t_right = self.space[Location - 9]
+		self._current.adjacents = [top, bot, left, right, b_left, b_right, t_left, t_right]
--- Game.py
+++ Game.py
@@ -2,15 +2,25 @@ import pygame
 from Astar import *
 
 def main():
+	xd = []
+	yd = []
 	searchSpace = []
 	for x in range(10):
+		xd.append(x)
 		for y in range(10):
 			n = Node(x, y)
-			n.walkable = True
+			n.walkable = False if (x >= 5 and x <= 6 and y >= 5 and y <= 8) else True
 			searchSpace.append(n)
-
-	pygame.init()
+			yd.append(y)
+			
+	Start = searchSpace[0]
+	Goal = searchSpace[99]
 	
+	for a in searchSpace:
+		a.setH(Goal)
+	
+	pygame.init()
+	id = (xd,yd)
 	WINDOW_SIZE = [255, 255]
 	screen = pygame.display.set_mode(WINDOW_SIZE)
 	
@@ -22,14 +32,14 @@ def main():
 	for i in searchSpace:
 		i.draw(screen, (255, 255, 255))
 		
-	Start = searchSpace[0]
-	Goal = searchSpace[99]
-	Traveler = Astar(searchSpace, Start, Goal)
+	Traveler = Astar(searchSpace, Start, Goal, id)
 	
 	# -----------------------------------------------------
 	while not done:
 		
 		clock.tick(60)
+		pygame.display.flip()
+		Traveler.Run()
 		pygame.display.flip()
 	pygame.quit()
 	
